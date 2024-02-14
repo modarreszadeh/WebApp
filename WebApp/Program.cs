e3d17bd518c3a19ccf0c9f58852e118252537e3f
modarreszadeh <modarreszadehmohammad@gmail.com>
2024-02-14 17:01:38 +0330

    some refactor

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,12 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHttpClient();
+
 var app = builder.Build();
 
-app.MapGet("/test", async () =>
+app.MapGet("/", async context =>
 {
-    await Task.Delay(20);
+    var httpClientFactory = context.RequestServices.GetRequiredService<IHttpClientFactory>();
+    var httpClient = httpClientFactory.CreateClient();
+    httpClient.Timeout = TimeSpan.FromMilliseconds(500);
 
-    Console.WriteLine("test endpoint");
+    await httpClient.GetAsync(app.Configuration["TEST_SERVER_URL"]);
 });
 
 app.Run();
